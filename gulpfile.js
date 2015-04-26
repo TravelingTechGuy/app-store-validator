c3db684129e85f10690c8584618cfbb390ee985d
Guy Vider <guy@guyvider.com>
2015-04-25 18:43:38 -0700

    added ESLint

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var babel = require('gulp-babel');
+var eslint = require('gulp-eslint');
+var baseDir = 'src/**/*.js';
 
-gulp.task('default', function() {
-	return gulp.src('src/**/*.js')
+gulp.task('lint', function () {
+    return gulp.src(baseDir)
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError());
+});
+
+gulp.task('compile', function() {
+	return gulp.src(baseDir)
     	.pipe(babel())
         .pipe(gulp.dest('lib'));
 });
+
+gulp.task('default', ['lint', 'compile']);
 
 gulp.task('test', ['default'], function() {
     return gulp.src('test/*.js', {read: false})
